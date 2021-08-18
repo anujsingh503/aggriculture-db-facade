ed04483538ff60ec8b3063274dbcca466f3f757f
Anuj Singh <anujs503@gmail.com>
2021-08-19 00:13:43 +0100

    Project initialized

---
+++ src/main/java/com/db/AggricultureDbFacadeApplication.java
@@ -0,0 +1,13 @@
+package com.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggricultureDbFacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggricultureDbFacadeApplication.class, args);
+	}
+
+}
